435eb617a59279fe4ce60b7c7d4695ab6fcb5195
Charles Yarnold <charlesyarnold@gmail.com>
2016-05-02 18:07:26 +0100

    Add call back, comment code

--- LUMOS-Code.ino
+++ LUMOS-Code.ino
@@ -7,7 +7,7 @@
 #include <Ticker.h>
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
-  #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
 //// Pin Settings - LUMOS
@@ -67,30 +67,47 @@ void setup()
   strip.show(); // Initialize all pixels to 'off'
 
   // Force wifi connection portal or attempt to connect
-  if(!digitalRead(btnPin)){
+  if (!digitalRead(btnPin)) {
+    // Force AP mode with captive portal for wifi setup
     strip.setPixelColor(0, strip.Color(50, 0, 50));
     strip.show();
     WiFiManager wifiManager;
     wifiManager.startConfigPortal(nodeName);
+    // Reset to use new settings
     ESP.reset();
+    delay(1000);
   }
-  else{
+  else {
+    // Try to connect with existing wifi settings
     strip.setPixelColor(0, strip.Color(0, 0, 50));
     strip.show();
     WiFiManager wifiManager;
-    wifiManager.autoConnect(nodeName);
+    // Add callback to change status neopixel to orange if connection fails and we enter AP mode
+    wifiManager.setAPCallback(configModeCallback);
+    // Attempt connection
+    if (!wifiManager.autoConnect(nodeName)) {
+      Serial.println("failed to connect and hit timeout");
+      // Reset and try again
+      strip.setPixelColor(0, strip.Color(50, 0, 0));
+      strip.show();
+      delay(1000);
+      strip.setPixelColor(0, strip.Color(0, 0, 0));
+      strip.show();
+      ESP.reset();
+      delay(1000);
+    }
   }
 
-  
+
   // Start up artnetnode library
   artnetnode.setName(nodeName);
   artnetnode.setStartingUniverse(1);
-  while(artnetnode.begin(1) == false){
+  while (artnetnode.begin(1) == false) {
     Serial.print("X");
   }
   Serial.println();
   Serial.println("Connected");
-  artnetnode.setDMXOutput(0,1,0);
+  artnetnode.setDMXOutput(0, 1, 0);
 
   // Connected and happy, flash green
   strip.setPixelColor(0, strip.Color(0, 50, 0));
@@ -101,21 +118,20 @@ void setup()
 
   // Decrease range - FIXME
   analogWriteRange(255);
-  
+
   // Setup heartbeat packet
   UdpSend.begin(4000);
   localIP = WiFi.localIP();
-  ticker.attach(5,beat);
+  ticker.attach(5, beat);
   beat();
 }
 
 void loop()
-{ 
+{
   uint16_t code = artnetnode.read();
-  if(code){
+  if (code) {
     if (code == OpDmx)
     {
-      //Serial.print("D");
       analogWrite(pinR, artnetnode.returnDMXValue(0, 1));
       analogWrite(pinG, artnetnode.returnDMXValue(0, 2));
       analogWrite(pinB, artnetnode.returnDMXValue(0, 3));
@@ -124,15 +140,27 @@ void loop()
       Serial.println("Art Poll Packet");
     }
   }
-  if (WiFi.status() != WL_CONNECTED){
+  if (WiFi.status() != WL_CONNECTED) {
+    // Lost wifi connection, reset to reconnect
     ESP.reset();
+    delay(1000);
   }
 }
 
-void beat(){
-
-  UdpSend.beginPacket({192,168,0,100},33333);
+void beat() {
+  // Send heartbeat packet
+  UdpSend.beginPacket({192, 168, 0, 100}, 33333);
   sprintf(udpBeatPacket, "{\"mac\":\"%x:%x:%x:%x:%x:%x\",\"ip\":\"%d.%d.%d.%d\",\"voltage\":%d}", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], localIP[0],  localIP[1],  localIP[2],  localIP[3], analogRead(A0));
-  UdpSend.write(udpBeatPacket, sizeof(udpBeatPacket)-1);
+  UdpSend.write(udpBeatPacket, sizeof(udpBeatPacket) - 1);
   UdpSend.endPacket();
+}
+
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+  // Change status led
+  strip.setPixelColor(0, strip.Color(50, 50, 0));
+  strip.show();
 }
